6e76ca68d08c9d578d4096782a07280ecd3d33ca
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2022-07-21 17:11:38 +0100

    variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+console.log(process.env.FIREBASE_API_KEY)
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Demo Firebase Authentication`,
