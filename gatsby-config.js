df11c6043b4eb6ddf324d82a9b11fb8d7dde2d92
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2022-07-21 16:35:49 +0100

    Env variables setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Demo Firebase Authentication`,
   },
   plugins: [`gatsby-plugin-react-helmet`],
-}
+};
